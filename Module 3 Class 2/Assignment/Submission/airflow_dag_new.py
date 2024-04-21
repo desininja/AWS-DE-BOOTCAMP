78d4052aaf2e07024b5d651b32457db44abd180b
Himanshu Bhatt <66287451+desininja@users.noreply.github.com>
2024-04-21 16:44:01 +0530

    SUbmission file

--- Module 3 Class 2/Assignment/Submission/airflow_dag_new.py
+++ Module 3 Class 2/Assignment/Submission/airflow_dag_new.py
@@ -3,16 +3,18 @@ from airflow.providers.amazon.aws.operators.emr import EmrAddStepsOperator
 from airflow.providers.amazon.aws.sensors.emr import EmrStepSensor
 from datetime import datetime
 
+
 dag = DAG(
-    'submit_pyspark_job_to_emr',
+    'submit_pyspark_job_to_emr_food_delivery_project',
     start_date=datetime(2021, 1, 1),
     catchup=False,
     tags=['test'],
 )
 
+
 step_adder = EmrAddStepsOperator(
     task_id='add_step',
-    job_flow_id='j-2HJM11AYZLN88',
+    job_flow_id='j-7MILHQSIRJDA',
     aws_conn_id='aws_default',
     steps=[{
         'Name': 'Run PySpark Script',
@@ -23,7 +25,7 @@ step_adder = EmrAddStepsOperator(
                 'spark-submit',
                 '--deploy-mode',
                 'cluster',
-                's3://pyspark-code-gds1/s3_to_hive_spark.py',
+                's3://pyspark-scripts-for-projects/food-delivery/pyspark-scripts/pyspark_job.py',
             ],
         },
     }],
@@ -32,7 +34,7 @@ step_adder = EmrAddStepsOperator(
 
 step_checker = EmrStepSensor(
     task_id='check_step',
-    job_flow_id='j-2HJM11AYZLN88',
+    job_flow_id='j-7MILHQSIRJDA',
     step_id="{{ task_instance.xcom_pull(task_ids='add_step', key='return_value')[0] }}",
     aws_conn_id='aws_default',
     poke_interval=120,  # Check every 2 minutes
